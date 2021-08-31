23217d4d996bec36fdbb94afa5fae8646358e1d0
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-08-31 23:22:21 +0100

    Create lualib.h

---
+++ lua/lualib.h
@@ -0,0 +1,20 @@
+/*
+** Libraries to use in LUA and PLP programs
+** Technology Group in Computer Graphics
+*/
+#ifndef lualib_h
+#define lualib_h
+
+void iolib_open   (void);
+void strlib_open  (void);
+void mathlib_open (void);
+
+
+/* auxiliar functions (private) */
+void lua_arg_error(char *funcname);
+char *lua_check_string (int numArg, char *funcname);
+float lua_check_number (int numArg, char *funcname);
+char *lua_opt_string (int numArg, char *def, char *funcname);
+#define lua_opt_int(n,d)	((int)lua_opt_number(n,d))
+
+#endif 
