1de2cce56646c35fcb2450aac9f65fda71d21b05
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-08-31 23:19:42 +0100

    Create inout.h

---
+++ lua/inout.h
@@ -0,0 +1,24 @@
+ 
+/*
+** inout.h
+**
+** Cunknown
+*/
+
+#include "../import"
+#ifndef inout_h
+#define inout_h
+
+extern int lua_linenumber;
+extern int lua_debug;
+extern int lua_debugline;
+
+char *lua_openfile    (char *fn);
+void lua_closefile    (void);
+int  lua_openstring   (char *s);
+void lua_closestring  (void);
+int  lua_pushfunction (int file, int function);
+void lua_popfunction  (void);
+void lua_reportbug    (char *s);
+
+#endif
