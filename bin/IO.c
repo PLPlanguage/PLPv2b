4b695be12323163ae24d99c48bc6c1389ff0688c
Cunknown <PLPlanguage@hotmail.com>
2021-09-01 00:33:49 +0100

    IO.c  for installation PLP on System

---
+++ bin/IO.c
@@ -0,0 +1,93 @@
+#include <IO.h>
+
+in plp_error($ *s){
+    return fprintf(stderr,red "Error PLP|>" set "%s;\n",s);
+    exit(0);
+}
+
+in inout($ * a){
+    FILE * fp = fopen(a,"r");
+    FILE * fp2 = fopen("/tmp/cplp","wr");
+    cond fp == NULL || fp2 == NULL){
+    	lua_error("NILL");
+    	remove("/tmp/cplp");
+    	exit(0);
+    }
+     $ c[256];
+    fputs("#include \"/usr/bin/import\"\n",fp2);
+     in cs;
+    when !feof(fp)){
+    	cs= getc(fp);
+    	cond cs == EOF) break;
+    	 cond cs == ':'){ ungetc(';',fp); cs++; };
+          cond cs == ' ' || cs == '\t') ungetc(';',fp);
+           cond cs == '.'){ ungetc(')',fp); continue; }
+            cond cs == '|') ungetc('#',fp);
+           	 other ungetc(cs,fp);
+            cond fgets(c,256,fp) != NULL) fputs(c,fp2);
+            	other break;
+    }
+    fclose(fp);
+    fclose(fp2);
+    return 0;
+}
+
+in argvinout($ * s){
+    FILE * fp2 = fopen("/tmp/cplp","wr");
+        fputs("#include \"/usr/bin/import\"\n#line 1\n _ ",fp2);
+        fputs(s,fp2);
+     	fputs("\nDone",fp2);
+        fclose(fp2);
+        print(nline);
+	return 0;
+}
+
+in defkey($ * i){
+	FILE * fp = fopen("/usr/bin/Define","a");
+	fputs("#define ",fp);
+	fprintf(fp,Str$ i);
+	fprintf(fp,nline);
+	fclose(fp);
+	print(nline);
+	return 0;
+}
+
+in cc(out){
+     $ str2[512];
+    strcpy(str2,"cc ");
+    strcat(str2,"/tmp/cplp");
+    strcat(str2,".c -O2 -I. -c");
+    strcat(str2," -o /tmp/aout");
+    system(str2);
+     $ str3[512]; 
+    strcpy(str3,"c++ ");
+    FILE * aout = fopen("/tmp/aout","r");
+    cond aout != NULL)
+    strcat(str3,"/tmp/aout /usr/bin/cmath.o /usr/bin/IO.o /usr/bin/luasyntax.o /usr/bin/lib.o /usr/bin/cs50.o /usr/bin/lua/hash.o /usr/bin/lua/inout.o /usr/bin/lua/lex_yy.o /usr/bin/lua/opcode.o /usr/bin/lua/table.o /usr/bin/lua/y_tab.o /usr/bin/lua/iolib.o /usr/bin/lua/mathlib.o /usr/bin/lua/strlib.o -lm");
+    other {
+    plp_error("NULL");
+    remove("/tmp/cplp.c");
+    exit(0);
+    }
+    return system(str3);
+}
+
+in cpp(out){
+     $ str4[512];
+    strcpy(str4,"c++ ");
+    strcat(str4,"/tmp/cplp");
+    strcat(str4,".cpp -O2 -I. -c");
+    strcat(str4," -o /tmp/aout");
+    system(str4);
+     $ str5[512]; 
+    strcpy(str5,"c++ ");
+    FILE * aout = fopen("/tmp/aout","r");
+    cond aout != NULL)
+    strcat(str5,"/tmp/aout /usr/bin/cmath.o /usr/bin/IO.o /usr/bin/luasyntax.o /usr/bin/lib.o /usr/bin/cs50.o /usr/bin/lua/hash.o /usr/bin/lua/inout.o /usr/bin/lua/lex_yy.o /usr/bin/lua/opcode.o /usr/bin/lua/table.o /usr/bin/lua/y_tab.o /usr/bin/lua/iolib.o /usr/bin/lua/mathlib.o /usr/bin/lua/strlib.o -lm");
+    other {
+    plp_error("NULL");
+    remove("/tmp/cplp.cpp");
+    exit(0);
+    }
+    return system(str5);
+}
