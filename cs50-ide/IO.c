a7f537ce24179ac534c19a460aaa8c545396c765
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-09-04 23:08:52 +0100

    IO.c error fixed on cs50 ide

--- cs50-ide/IO.c
+++ cs50-ide/IO.c
@@ -67,7 +67,7 @@ in cc(out){
     strcpy(str3,"c++ ");
     FILE * aout = fopen("/tmp/aout","r");
     cond aout != NULL)
-    strcat(str3,"-no-pie /tmp/aout ~/PLPv2b/cmath.o ~/PLPv2b/IO.o ~/PLPv2b/luasyntax.o ~/PLPv2b/lib.o ~/PLPv2b/cs50.o ~/PLPv2b/lua/hash.o ~/PLPv2b/lua/inout.o ~/PLPv2b/lua/lex_yy.o ~/PLPv2b/lua/opcode.o ~/PLPv2b/lua/table.o ~/PLPv2b/lua/y_tab.o ~/PLPv2b/lua/iolib.o ~/PLPv2b/lua/mathlib.o ~/PLPv2b/lua/strlib.o -lm");
+    strcat(str3,"-no-pie /tmp/aout ../PLPv2b/cmath.o ../PLPv2b/IO.o ../PLPv2b/luasyntax.o ../PLPv2b/lib.o ../PLPv2b/cs50.o ../PLPv2b/lua/hash.o ../PLPv2b/lua/inout.o ../PLPv2b/lua/lex_yy.o ../PLPv2b/lua/opcode.o ../PLPv2b/lua/table.o ../PLPv2b/lua/y_tab.o ../PLPv2b/lua/iolib.o ../PLPv2b/lua/mathlib.o ../PLPv2b/lua/strlib.o -lm");
     other {
     plp_error("NULL");
     remove("/tmp/cplp.c");
@@ -87,7 +87,7 @@ in cpp(out){
     strcpy(str5,"c++ ");
     FILE * aout = fopen("/tmp/aout","r");
     cond aout != NULL)
-    strcat(str5,"-no-pie /tmp/aout ~/PLPv2b/cmath.o ~/PLPv2b/IO.o ~/PLPv2b/luasyntax.o ~/PLPv2b/lib.o ~/PLPv2b/cs50.o ~/PLPv2b/lua/hash.o ~/PLPv2b/lua/inout.o ~/PLPv2b/lua/lex_yy.o ~/PLPv2b/lua/opcode.o ~/PLPv2b/lua/table.o ~/PLPv2b/lua/y_tab.o ~/PLPv2b/lua/iolib.o ~/PLPv2b/lua/mathlib.o ~/PLPv2b/lua/strlib.o -lm");
+    strcat(str5,"-no-pie /tmp/aout ../PLPv2b/cmath.o ../PLPv2b/IO.o ../PLPv2b/luasyntax.o ../PLPv2b/lib.o ../PLPv2b/cs50.o ../PLPv2b/lua/hash.o ../PLPv2b/lua/inout.o ../PLPv2b/lua/lex_yy.o ../PLPv2b/lua/opcode.o ../PLPv2b/lua/table.o ../PLPv2b/lua/y_tab.o ../PLPv2b/lua/iolib.o ../PLPv2b/lua/mathlib.o ../PLPv2b/lua/strlib.o -lm");
     other {
     plp_error("NULL");
     remove("/tmp/cplp.cpp");
