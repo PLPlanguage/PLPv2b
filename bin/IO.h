75181157c5597b2835f401e511b8250ca7022252
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-07-22 00:13:29 +0330

    Released on 2021

---
+++ bin/IO.h
@@ -0,0 +1,26 @@
+/* The Project Programming Language - PLP
+ The Second Release on PLP Language -> PLPv2b
+ BSD License and MIT License
+ Time: 21/08/01 00:00 GMT
+*/
+
+#include "Define"
+#include "Type"
+
+  #ifndef IO_H
+    #define IO_H
+in  plp_error ($ * s);
+in  inout     ($ * a);
+in  argvinout ($ * s);
+in  defkey    ($ * i);
+in  cc	      (out);
+in  cpp       (out);
+
+out define    ($ * report);
+    #define Windows(_WIN32)
+  #ifndef Windows
+  #undef
+    #define Windows(_WIN64)
+  #endif
+  
+  #endif
