1c2e8c33dab8f10b3aafbe366c0d3d63ed1b7654
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-12-15 16:09:54 -0500

    Released on 2021

--- cmath.h
+++ cmath.h
@@ -9,7 +9,11 @@
 
 #include "Type"
 #include "Define"
+#ifndef __cplusplus
 #include <math.h>
+#else
+#include <cmath>
+#endif
     
     #define PI          acos(0.0)*2
     #define Euler	exp(1)
@@ -18,13 +22,19 @@
     #define deg(a) ((a)*180.0/PI)
     #define rad(a)    ((a)*PI/180.0)
     #define radius(a)   (a*a)*PI
-    
+#ifndef __cplusplus
+    #define eulers(theta) (creal(cos(theta)) + cimag(sin(theta)))
+#endif
+#ifdef __cplusplus
+    #define eulers(theta) (real(cos(theta)) + imag(sin(theta)))
+#endif
+#ifndef __cplusplus
     #define min(X, Y) (((X) < (Y)) ? (X) : (Y))
     #define max(a,b) \
     ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
-     
+#endif
   #ifdef __cplusplus
 	extern "C"{
   #endif
@@ -39,6 +49,7 @@ dec4 ASin           (in AS);
 dec4 Exp            (dec4 E);
 dec4 Frexp          (dec4 X);
 dec4 derivative     (dec4 x, dec4 n);
+dec8 integral       (dec8 x, dec8 n);
 dec4 pi		    (dec4 i,dec4 n,dec4 x);
 dec4 sigma	    (dec4 i,dec4 n,dec4 x);
 in   matrix	    (in row,in column,in *mat);
@@ -46,6 +57,8 @@ in   ack	    (in m, in n);
 in   mod	    (in d1,in d2);
 in   gcd	    (in a,in b);
 in   lcm	    (in a,in b);
+in   fib	    (in n);
+#define fibonacci(n) fib(n - 1)
 in   factorial      (in c);
 in   collatz        (in n);
 ULIN catalan        (UIN n);
